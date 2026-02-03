3ec1737a2b52ffd18caa0658dd8f0a82a1ff49c5
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-03 10:26:53 -0500

    Finish wall collision

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,18 +4,19 @@ extends CharacterBody2D
 
 @export var can_move := false
 
-@export var horizontal_speed := 200.0
+@export var horizontal_speed := 300.0
 
-@export var vertical_strength := 2000.0
+@export var vertical_strength := 750.0
 
 @export var max_vertical_speed := 1000.0
 
-@export var gravity := 5000.0
+@export var gravity := 2000.0
 
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and !can_move:
 		can_move = true
+		self.velocity.x = horizontal_speed
 
 
 func _physics_process(delta: float) -> void:
@@ -23,14 +24,14 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	if Input.is_action_just_pressed("jump"):
-		self.velocity.y -= vertical_strength
+		self.velocity.y = -vertical_strength
 	
 	self.velocity.y += delta * gravity
-	self.velocity.x = horizontal_speed
 	
 	self.velocity.y = clamp(self.velocity.y, -max_vertical_speed, max_vertical_speed)
 	
 	var collision := move_and_collide(self.velocity * delta)
 	
 	if collision:
-		self.velocity.bounce(collision.get_normal())
+		if collision.get_collider() is Wall:
+			self.velocity = self.velocity.bounce(collision.get_normal())
